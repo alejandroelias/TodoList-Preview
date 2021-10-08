5c36ead3b80b38f621e15818dc542ec7068bcc14
Alejandro Elias <alfredo.elias.o@gmail.com>
2021-10-08 09:53:23 -0600

    Modificaciones hasta el vie. 8-oct.-21 09:53 a. m.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Padre } from './components/07-tarea-memo/Padre';
+// import { MemoHook } from './components/06-memos/MemoHook';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
@@ -11,6 +14,6 @@ import { Layout } from './components/05-useLayoutEffect/Layout';
 // import {HookApp} from './HookApp'
 
 ReactDOM.render(
-    <Layout   />,
+    <Padre />,
   document.getElementById('root')
 );
